193cbc39a0558f616a79095c2f2ffc6422e4a204
Larry Person <lp10011@gmail.com>
2019-09-06 14:02:56 -0400

    our future works and catches an injected failiure

--- src/lib/spotify_future.rs
+++ src/lib/spotify_future.rs
@@ -1,6 +1,6 @@
 pub mod spotify_future {
 
-    use futures::{future, future::finished, prelude::*, Future, Poll};
+    use futures::{future, future::finished, prelude::*, Future, Poll };
     use hyper::Client;
     use hyper::{Body, Request, Response};
     use hyper_tls::HttpsConnector;
@@ -15,12 +15,13 @@ pub mod spotify_future {
     }
 
     impl SpotifyFuture {
+        
         pub fn new(auth_code: &String, what_to_get: &str, timeframe: &str) -> Self {
             let https = HttpsConnector::new(4).unwrap();
             let client = Client::builder().build::<_, hyper::Body>(https);
 
             let uri = format!(
-                    "https://api.spotify.com/v1/me/topxx/{}?limit=50&time_range={}",
+                    "https://api.spotify.com/v1/me/topx/{}?limit=50&time_range={}",
                     what_to_get, timeframe
                 );
 
@@ -35,23 +36,32 @@ pub mod spotify_future {
 
             let future = client
                 .request(request)
-                .map_err(|x| future::err(SimpleError::new("x")))
+                .map_err(|x| SimpleError::new("x"))
                 .and_then(move |result| {
                     println!("{}", result.status());
-                    if result.status().as_u16() == 404 {
-                        return future::err(SimpleError::new("parse error"));
+                    if [404u16, 400u16].into_iter().any(|x| result.status().as_u16() == *x ) {
+                        let this_return = future::err(SimpleError::new("400"));
+                        return this_return;
+                            //.map(|x| x)
+                            //.map_err(|x| x);
                     }
 
-                    result
+                    return future::ok(result);
+                })
+                .and_then(|result| {
+                    let transformed_result = result
                         .into_body()
                         .concat2()
-                        .map(|x| future::ok(String::from_utf8(x.to_vec())))
-                        .map_err(|x| future::err(SimpleError::new("x")))
-                })
-                //.map(|x| x)
-                .map_err(|x| 
-                   future::err(SimpleError::new("parse error"))
+                        .map(|x| String::from_utf8(x.to_vec()).unwrap())
+                        .map_err(|x| SimpleError::new("x"));
+
+                    return transformed_result;
+                }
                 );
+                //.map(|x| String::from("hey"))
+                //.map_err(|x| 
+                //   SimpleError::new("OH SHIT! NRECOVERABLE")
+                //);
 
             SpotifyFuture {
                 uri: uri,
@@ -68,7 +78,20 @@ pub mod spotify_future {
         type Error = SimpleError;
 
         fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-            return self.the_future.poll();
+            match self.the_future.poll() {
+                Ok(Async::Ready(t)) => {Ok(Async::Ready(t))}
+                Ok(Async::NotReady) => {Ok(Async::NotReady)}
+                Err(e) => { 
+                    println!("IN POLL ==> {:?}", e);
+                    if e.as_str() == "400" {
+                        println!("400 in poll");
+                        return Err(e);
+                    }
+
+                    return Err(e);
+                }
+
+            }
         }
     }
 }
