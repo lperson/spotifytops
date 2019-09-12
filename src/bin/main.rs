50b26a952c1a7ff79e876aa427c9595996f6e8ed
Larry Person <lp10011@gmail.com>
2019-09-12 17:28:41 -0400

    redirect to authorization if we don't know the token in the query

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,8 +5,9 @@ use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 use libspotifytops::app::spotify_login_callback;
 use libspotifytops::app::spotify_tops;
-use libspotifytops::spotify::auth::*;
+use libspotifytops::app::STATE;
 use libspotifytops::server;
+use libspotifytops::spotify::auth::*;
 use libspotifytops::CONFIG;
 
 use simple_error::SimpleError;
@@ -23,11 +24,20 @@ fn make_handler() -> Box<dyn FnMut(Request<Body>) -> BoxFut + Send> {
                 if let Some(query) = server::get_query(&req) {
                     if let Some(token) = query.get("t") {
                         if let Some(token) = token {
-                            return spotify_tops::handle(token.clone()); 
+                            let mut auth_code: Option<String> = None;
+                            {
+                                let mut tokens = STATE.tokens.lock().unwrap();
+                                if let Some(stored_auth_code) = tokens.get(&token) {
+                                    auth_code = Some(stored_auth_code.clone());
+                                }
+                            }
+                            if auth_code.is_some() {
+                                return spotify_tops::handle(&auth_code.unwrap());
+                            }
                         }
                     }
-                } 
-                
+                }
+
                 server::redirect(&mut response, &get_redirect(&req.uri().to_string()));
             }
 
@@ -46,12 +56,12 @@ fn make_handler() -> Box<dyn FnMut(Request<Body>) -> BoxFut + Send> {
 }
 
 fn main() {
-    let addr = format!("{}:{}", CONFIG.listen_addr, CONFIG.listen_port).parse().unwrap();
+    let addr = format!("{}:{}", CONFIG.listen_addr, CONFIG.listen_port)
+        .parse()
+        .unwrap();
 
     let server = Server::bind(&addr)
-        .serve(move || {
-            service_fn(make_handler())
-        })
+        .serve(move || service_fn(make_handler()))
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Listening on http://{}", addr);
